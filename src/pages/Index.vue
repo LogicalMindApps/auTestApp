a0a36e5dca700be7e210fceac8f06a83052f1d25
rk <logicalmind.dex@gmail.com>
2020-12-16 16:37:41 +0300

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page class="flex flex-center column q-pa-lg">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+
+    >
+    <div class="column col" style="padding-top: 20px">
+      <div class="col-auto">
+        isUpdateAvailable: {{ isUpdateAvailable }}
+      </div>
+      <div class="col-auto">
+        isUpdateDownloaded: {{ isUpdateDownloaded }}
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      isUpdateAvailable: false,
+      isUpdateDownloaded: false,
+    }
+  },
+  created () {
+    const { ipcRenderer } = require('electron')
+    ipcRenderer.on('update_available', () => {
+      ipcRenderer.removeAllListeners('update_available')
+    })
+    ipcRenderer.on('update_downloaded', () => {
+      ipcRenderer.removeAllListeners('update_downloaded')
+    })
+  }
+
+}
+</script>
